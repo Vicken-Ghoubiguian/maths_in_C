47a30a944272fb057bac634cdf035e519a99f6bb
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2023-11-07 10:19:49 +0100

    Implementation of 'sqrt by Heron' function

--- maths_in_C.c
+++ maths_in_C.c
@@ -3,6 +3,19 @@
 
 float sqrt_by_Heron(int a, int x)
 {
+    float sqrt_by_Heron_a = (float) a;
+    float sqrt_by_Heron_x = (float) x;
+    float last_iter_value = 0.0;
+
+    float sqrt_by_Heron_x = (sqrt_by_Heron_x + (sqrt_by_Heron_a / sqrt_by_Heron_x)) / 2;
+
+    do {
+
+        last_iter_value = sqrt_by_Heron_x;
+        sqrt_by_Heron_x = (sqrt_by_Heron_x + (sqrt_by_Heron_a / sqrt_by_Heron_x)) / 2;
+
+    } while(last_iter_value != sqrt_by_Heron_x);
+
     return 0.0;
 }
 
