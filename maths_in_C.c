f512075b2eb8c1703393fdc8cb5d36d8f80f02b5
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2023-11-07 10:47:15 +0100

    // for future comments

--- maths_in_C.c
+++ maths_in_C.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//
 float get_sqrt_by_Heron_x(float sqrt_by_Heron_a)
 {
-
+    //
     return 0.0;
 }
 
+//
 float sqrt_by_Heron(int a, int x)
 {
+    //
     float sqrt_by_Heron_a = (float) a;
     float sqrt_by_Heron_x = (float) x;
     float last_iter_value = 0.0;
 
+    //
     sqrt_by_Heron_x = (sqrt_by_Heron_x + (sqrt_by_Heron_a / sqrt_by_Heron_x)) / 2.0;
 
+    //
     do {
 
+        //
         last_iter_value = sqrt_by_Heron_x;
         sqrt_by_Heron_x = (sqrt_by_Heron_x + (sqrt_by_Heron_a / sqrt_by_Heron_x)) / 2.0;
 
+    //
     } while(last_iter_value != sqrt_by_Heron_x);
 
+    //
     return sqrt_by_Heron_x;
 }
 
+//
 int main()
 {
+    //
     printf("%.14f", sqrt_by_Heron(3.0, 1.0));
+
+    //
     return 0;
 }
