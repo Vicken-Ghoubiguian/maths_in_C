77af69ee28ebef7f6fc04d997970183eae766779
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2023-11-15 15:22:41 +0100

    Iteration factorial

--- maths_in_C.c
+++ maths_in_C.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//
+int iterative_factorial(int number)
+{
+    int reverse_iteration = number;
+    int returned_number = number;
 
+    do {
+
+        reverse_iteration--;
+        returned_number = returned_number * reverse_iteration;
+
+    } while(reverse_iteration > 1);
+
+    return returned_number;
+}
 
 //
 float get_sqrt_by_Heron_x(float sqrt_by_Heron_a)
@@ -49,7 +63,7 @@ float sqrt_by_Heron(int a, int x)
 int main()
 {
     //
-    printf("%.20f\n", sqrt_by_Heron(3.0, 1.0));
+    printf("%d\n", iterative_factorial(9));
 
     //
     return 0;
